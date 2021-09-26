e757314a4dd75e919f7ce314988b8a0c91d6ec7d
KayleyB001 <90276649+KayleyB001@users.noreply.github.com>
2021-09-26 19:43:12 -0400

    finish

--- CarGarage/Car.cs
+++ CarGarage/Car.cs
@@ -12,5 +12,85 @@ namespace CarGarage
         public int FuelLevel { get; set; }
         public bool AutoBrakeActive { get; set; }
 
+
+        public string SetMake(string make)
+        {
+            Make = "Honda";
+            return make;
+        }
+
+        public string SetModel(string model)
+        {
+            Model = "Accord";
+            return model;
+        }
+
+        public int GetSpeed()
+        {
+            Speed = 27;
+            return Speed;
+
+        }
+
+        public bool IsSpeeding()
+        {
+            if (Speed >= 70)
+            {
+                return true;
+            }
+            else
+                return false;
+
+
+        }
+
+        public int Accelerate()
+        {
+
+            if (Speed == 0)
+            {
+                Speed += 10;
+            }
+            if (Speed >= 100)
+            {
+                Speed = 100;
+            }
+
+            return Speed;
+
+
+
+        }
+
+        public int Brake()
+        {
+           
+
+            Convert.ToBoolean(Speed);
+            if (Speed == 10)
+            {
+                Speed -= 7;
+
+            }
+            if (AutoBrakeActive == true)
+            {
+                Speed -= 25;
+            }
+            if (Speed <= 1)
+            {
+                Speed = 0;
+            }
+            return Speed;
+        }
+            
+            
+
+
+
+
+
+
+
+
     }
 }
